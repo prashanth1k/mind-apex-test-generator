99e1f03208f9ba7914e359576ff9184acee94c48
prashanth <prashanth1krishnamurthy@gmail.com>
2021-06-27 12:57:49 +0530

    INIT

---
+++ extension.js
@@ -0,0 +1,68 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require("vscode");
+const fileUtil = require("./src/fileUtil");
+const generateTestApex = require("./src/generateTestApex");
+
+const thisDoc = vscode.workspace.openTextDocument;
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Extension "MindApexTestGenerator" is now active!');
+
+  let disposable = vscode.commands.registerCommand(
+    "mindapextestgenerator.createTestApex",
+    activateExtn
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+
+/*
+ custom functions
+*/
+function activateExtn() {
+  const editor = vscode.window.activeTextEditor;
+  if (!editor)
+    return vscode.window.showErrorMessage(
+      "No active editor. Run this command from valid Apex file."
+    );
+
+  if (editor.document.languageId != "apex" || editor.document.isUntitled)
+    return vscode.window.showErrorMessage(
+      "Invalid or unsaved file. Generate Apex from a valid Apex file."
+    );
+
+  const fileName = editor.document.fileName.substr(
+    0,
+    editor.document.fileName.lastIndexOf(".")
+  );
+
+  const testFileName = `${fileName}Test`;
+  const testApexContent = generateTestApex.genTestClass(
+    testFileName,
+    "pass_apex_content"
+  );
+  const testFile = fileUtil.createApexFile(testFileName, testApexContent);
+  console.log("testFile: ", testFile);
+
+  // all things done!
+  vscode.window.showInformationMessage(
+    "Successfully generated test class for Apex!"
+  );
+}
+
+module.exports = {
+  activate,
+  deactivate,
+};
---
+++ src/fileUtil.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+const vscode = require("vscode");
+
+const createApexFile = (filePath, fileData = "", openFile = true) => {
+  console.debug("Creating file at: ", filePath);
+
+  const apexTestXml = `
+  <?xml version="1.0" encoding="UTF-8"?>
+  <ApexClass xmlns="http://soap.sforce.com/2006/04/metadata">
+    <apiVersion>51.0</apiVersion>
+    <status>Active</status>
+  </ApexClass>
+  `;
+
+  const testFileName = `${filePath}.cls`;
+  fs.writeFile(testFileName, fileData, (err) => {
+    if (err) return false;
+    if (!fs.existsSync(`${testFileName}-meta.xml`)) {
+      fs.writeFile(`${testFileName}-meta.xml`, apexTestXml, (err) => {
+        console.debug(err);
+      });
+    }
+    console.debug("Created " + `${testFileName}`);
+    if (openFile) {
+      vscode.workspace
+        .openTextDocument(vscode.Uri.file(testFileName))
+        .then((f) =>
+          vscode.window.showTextDocument(f, vscode.ViewColumn.Beside)
+        );
+    }
+    return `${filePath}Test.cls`;
+  });
+};
+
+module.exports = {
+  createApexFile,
+};
